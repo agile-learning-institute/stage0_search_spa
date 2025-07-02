435abcea40d345657edd1e8de5fc353ab377b794
Mike Storey <mike@agile-learning.institute>
2025-07-02 17:40:07 -0400

    MVP complete

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Set default values for environment variables
+export SEARCH_SPA_PORT=${SEARCH_SPA_PORT:-8084}
+
 # Substitute environment variables in nginx configuration
 envsubst '${SEARCH_API_HOST} ${SEARCH_API_PORT} ${SEARCH_SPA_PORT}' < /etc/nginx/nginx.conf > /tmp/nginx.conf
 
